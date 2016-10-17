4fc9c74982f3378cc089b500c45bc103670044ec
A_2 <ahmed.y.allam@gmail.com>
2016-10-17 08:37:05 -0400

    .

--- pyseqlab/crf_learning.py
+++ pyseqlab/crf_learning.py
@@ -484,8 +484,8 @@ class Learner(object):
                     
                     y_original = seqs_info[seq_id]['flat_y']
                     T = seqs_info[seq_id]['T']
-#                     print("y original {}".format(y_original))
-#                     print("y imposter {}".format(y_imposter))
+                    print("y original {}".format(y_original))
+                    print("y imposter {}".format(y_imposter))
                     missmatch = [i for i in range(T) if y_original[i] != y_imposter[i]]
                     len_diff = len(missmatch)
                     if(len_diff):
