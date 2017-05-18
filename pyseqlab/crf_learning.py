157ba4b4baade108236548b49434b80ff30899e0
Ahmed Allam <ahmed.y.allam@gmail.com>
2017-05-18 16:38:14 -0400

    update for tutorials
    update for setting up beam_size while training models

--- pyseqlab/crf_learning.py
+++ pyseqlab/crf_learning.py
@@ -183,10 +183,10 @@ class Learner(object):
                 # setting beam size
                 beam_size = optimization_options.get("beam_size")
                 # default beam size
-                default_beam = self.crf_model.beam_size
+                default_beam = len(self.crf_model.model.Y_codebook)
                 if(type(beam_size) != int):
                     beam_size = default_beam
-                elif(beam_size < 0 or beam_size > default_beam):
+                elif(beam_size <= 0 or beam_size > default_beam):
                     beam_size = default_beam
                 optimization_config["beam_size"] = beam_size
                 self.crf_model.beam_size = beam_size
