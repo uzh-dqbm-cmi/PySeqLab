4ce6eb116179bdb8f0888910176e37607eebfcbb
A A <ahmed.y.allam@gmail.com>
2017-05-19 14:40:42 +0000

    conf.py edited

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -104,7 +104,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static', 'tutorials']
-html_static_path = ['_static', '../../tutorials/html']
+html_static_path = ['../../tutorials/html']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
